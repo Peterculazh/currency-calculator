7d05dc23305d753009f1291687700762991fc719
Peter Kulazhenko <peterculazh@gmail.com>
2020-12-16 13:58:37 +0200

    improved outlook and make clear that there is sorting workin

--- src/components/Currencies/CurrenciesList.tsx
+++ src/components/Currencies/CurrenciesList.tsx
@@ -1,8 +1,18 @@
-import { ICurrency, setSort } from '../../redux/reducers/currencies';
+import { ICurrency, ICurrencySort, setSort } from '../../redux/reducers/currencies';
 import CurrencyRow from './CurrencyRow';
 import './CurrencyList.sass';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
+
+const isSorting = (sort: ICurrencySort, fieldName: string) => fieldName === sort.field ? true : false;
+
+const getDirectionSort = (sort: ICurrencySort) => {
+    if (sort.sort === -1) {
+        return "item-descending";
+    } else if (sort.sort === 1) {
+        return "item-ascending";
+    }
+}
 
 const CurrenciesList = (
     { currencies }:
@@ -36,9 +46,15 @@ const CurrenciesList = (
     return (
         <div className="currencies-list">
             <div className="currencies-list-header">
-                <div className="item" onClick={() => sortBy("name")}>Currency name</div>
-                <div className="item" onClick={() => sortBy("sale")}>Sale</div>
-                <div className="item" onClick={() => sortBy("buy")}>Buy</div>
+                <div className={`item ${isSorting(sort, "name") ? getDirectionSort(sort) : ""}`} onClick={() => sortBy("name")}>
+                    <span>Currency name</span>
+                </div>
+                <div className={`item ${isSorting(sort, "sale") ? getDirectionSort(sort) : ""}`} onClick={() => sortBy("sale")}>
+                    <span>Sale</span>
+                </div>
+                <div className={`item ${isSorting(sort, "buy") ? getDirectionSort(sort) : ""}`} onClick={() => sortBy("buy")}>
+                    <span>Buy</span>
+                </div>
             </div>
             <div className="currencies-list-content">
                 {currencies.map(item => <CurrencyRow key={item.name} currency={item} />)}
